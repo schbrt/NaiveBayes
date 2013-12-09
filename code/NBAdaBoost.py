0a1338c5b63e2019eba3375c2a0ec4fb86d8ceed
Michael Schubert <schubert.michael.j@gmail.com>
2013-12-09 14:18:05 -0600

    fixed error

--- code/NBAdaBoost.py
+++ code/NBAdaBoost.py
@@ -9,7 +9,6 @@ class AdaBoost:
         self.train_data = train_data
         self.train_len = len(train_data)
         self.weights = np.ones(self.train_len)/self.train_len
-        self.rules = []
 
         self.correct_weights = []
         self.incorrect_weights = []
@@ -20,7 +19,7 @@ class AdaBoost:
         sample = np.random.choice(self.train_data, self.train_len, replace = True, p = self.weights)
         return sample
 
-    def update(self, classifier):
+    def update(self, error_rate, classifier):
         alpha = .5 * math.log((1-error_rate)/float(error_rate))
 
         for idx in self.correct_weights:
@@ -36,40 +35,45 @@ class AdaBoost:
 
     def normalize_weights(self):
         tot = float(sum(self.weights))
-        for idx in range(len(weights)):
-            weights[idx] = weights[idx]/tot
+        for idx in range(len(self.weights)):
+            self.weights[idx] = self.weights[idx]/tot
 
     def reset_weights(self):
         for item in weights:
             item = 1/self.train_len
 
     def error(self):
-        return sum(self.incorrect_weights)
+        sum = 0
+        for i in self.incorrect_indicies:
+            sum += self.weights[i]
 
-    def test(test_data, pos, neg, cl, error_rate):
+        return sum
+
+    def test(self, test_data, pos, neg, cl, error_rate):
         if error_rate < .5:
             test_ret = []
             count = 0
-            self.correct_weights = []
-            self.incorrect_weights = []
+            self.correct_indicies = []
+            self.incorrect_indicies = []
 
             for line in test_data:
                 val = cl.classify(line, pos, neg)
 
                 if val[0] != val[1]:
-                    self.incorrect_weights.append(count)
+                    self.incorrect_indicies.append(count)
+
                 else:
-                    self.correct_weights.append(count)
+                    self.correct_indicies.append(count)
 
                 test_ret.append(val)
                 count += 1
 
-            self.update(error_rate ,cl)
+            error_rate = self.error()
+            self.update(error_rate, cl)
 
         else:
-            self.reset_weights
+            self.reset_weights()
 
-    def error(self, sample, classifier):
 
 
 args = sys.argv
@@ -78,15 +82,17 @@ test_file = '../' + args[2]
 
 
 train_data = NB.read(train_file)
-AB = AdaBoost(train_data)
+ab = AdaBoost(train_data)
 error_rate = 0
 #ye olde iteration begins here
 for i in xrange(20):
     #create sample data set
-    sample = AB.sample()
+    sample = ab.sample()
     #build classifier
     pos, neg, cl = NB.train(sample)
 
     ab.test(sample, pos, neg, cl, error_rate)
+
+print ab.ensemble
 
 
